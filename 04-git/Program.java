a987ee5aacd31f6645dbe35cb7c1daa5210b0a23
Kacper <72124062+Kacper-kdr@users.noreply.github.com>
2024-10-29 14:52:32 +0100

    Update Program.java

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,8 +1,8 @@
 public class Main {
   int x = 5;
   public static void main(String[] args) {
-    Main myObj1 = new Main();  // Object 1
-    Main myObj4 = new Main();  // Object 2
+    Main myObj1 = new Main();
+    Main myObj4 = new Main();
     System.out.println(myObj1.x);
     System.out.println(myObj4.x);
   }
